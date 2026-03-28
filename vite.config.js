b39f9b55d457d65e0fff9d9d05b4dfa5b3fb7f21
oozaole <oozaole@gmail.com>
2026-03-28 15:35:49 +0800

    Optimize Vite build by splitting chunks into smaller pieces

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,24 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+          if (id.includes('src/data/conjugation_tables')) {
+            return 'data-tables';
+          }
+          if (id.includes('src/data/questions')) {
+            return 'data-questions';
+          }
+          if (id.includes('src/data/verb')) {
+            return 'data-verb';
+          }
+        }
+      }
+    }
+  }
 })
